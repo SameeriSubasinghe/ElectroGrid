2e5c29e4063532f9659606d1b2284ad9b36d0faf
Pathirana D.P.C.H <87385894+it20161088@users.noreply.github.com>
2022-04-22 21:42:29 +0530

    crud insert
    crud insert

---
+++ ElectroGridPAF/UserService/src/main/java/model/Registration.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Registration {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/epower?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insertRegister(String regName, String regAddress, String regEmail, String regDate, String regPNo) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into registerapi(`regID`,`regName`,`regAddress`,`regEmail`,`regDate`,`regPNo`)"
+					+ " values (?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, regName);
+			preparedStmt.setString(3, regAddress);
+			preparedStmt.setString(4, regEmail);
+			preparedStmt.setString(5, regDate);
+			preparedStmt.setString(6, regPNo);
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the customer.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+}
+
+	
